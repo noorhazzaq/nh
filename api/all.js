8840dc461724ae20f14c06d2299886c1f8995ec0
noorhazzaq <hazzaqn@gmail.com>
2026-02-08 03:08:31 +0800

    Update all.js

--- api/all.js
+++ api/all.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     }
     const data = await response.json();
     res.status(200).json(data);
-  } catch (err) {
+  } catch {
     res.status(200).json([]);
   }
 }
