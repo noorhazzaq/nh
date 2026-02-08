0902d4a6cd985341bc75ab5c57bd031deb802071
noorhazzaq <hazzaqn@gmail.com>
2026-02-09 00:31:17 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,18 @@
+import { saveMessage } from "./actions"
+
 export default function Home() {
+  async function handleSubmit(formData) {
+    "use server"
+    await saveMessage(formData.get("msg"))
+  }
+
   return (
     <main>
       <h1>Hello Hazzaq Style!</h1>
-      <p>Next.js App Router minimal di Vercel</p>
+      <form action={handleSubmit}>
+        <input name="msg" placeholder="Tulis mesej Hazzaq Style" />
+        <button type="submit">Simpan</button>
+      </form>
     </main>
   )
 }
