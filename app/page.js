aaf5428628b3cabc6d6f27bf267aab4bd97665ea
noorhazzaq <hazzaqn@gmail.com>
2026-02-09 00:41:58 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1>Hello Hazzaq Style!</h1>
-      <p>Next.js App Router di Node.js 24.x</p>
+      <p>Next.js App Router berjalan di Node.js 24.x di Vercel</p>
     </main>
   )
 }
