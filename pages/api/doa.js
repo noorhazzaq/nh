e13025b8ef67e8302fa20a7d988112131a883875
noorhazzaq <hazzaqn@gmail.com>
2026-02-09 00:17:51 +0800

    Delete doa.js

--- pages/api/doa.js
+++
@@ -1,6 +0,0 @@
-import { sql } from '@vercel/postgres';
-
-export default async function handler(req, res) {
-  const { rows } = await sql`SELECT * FROM doa ORDER BY tarikh DESC;`;
-  res.json(rows);
-}
