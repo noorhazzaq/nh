c788ee8ccccbe4ecf83e804cdb2d512092df9b70
noorhazzaq <hazzaqn@gmail.com>
2026-02-09 00:56:11 +0800

    Delete route.js

--- app/api/hello/route.js
+++
@@ -1,5 +0,0 @@
-export async function GET() {
-  return new Response(JSON.stringify({ message: "Hazzaq Style API di Node.js 24.x" }), {
-    headers: { "Content-Type": "application/json" },
-  })
-}
