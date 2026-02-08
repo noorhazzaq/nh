9a9c7fcbc64a8163d7cc3d9c25eb15e9babaf371
noorhazzaq <hazzaqn@gmail.com>
2026-02-09 00:37:18 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,18 +1,8 @@
-import { saveMessage } from "./actions"
-
 export default function Home() {
-  async function handleSubmit(formData) {
-    "use server"
-    await saveMessage(formData.get("msg"))
-  }
-
   return (
     <main>
       <h1>Hello Hazzaq Style!</h1>
-      <form action={handleSubmit}>
-        <input name="msg" placeholder="Tulis mesej Hazzaq Style" />
-        <button type="submit">Simpan</button>
-      </form>
+      <p>Next.js App Router di Node.js 24.x</p>
     </main>
   )
 }
